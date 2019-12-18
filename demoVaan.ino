6df31401944f43fb7e89d4afb103363775c4346f
Jani Lehto <jani.lehto@ictpro.labra>
2019-12-18 12:41:41 +0200

    Painike kunnossa

--- demoVaan.ino
+++ demoVaan.ino
@@ -1,17 +1,31 @@
 #define LED 6
 #define PAINIKE 5
-
+#define LEDON HIGH
+#define LEDOFF LOW
+#define TASO1 180
+#define TASO2 100
+#define FILTER 20
+int oldState;//Painikkeen tilojen seurantaa varten
 void setup(){
   pinMode(LED, OUTPUT);
   pinMode(PAINIKE, INPUT_PULLUP);
+  oldState = digitalRead(PAINIKE);//Alustetaan "muistijälki"
+  Serial.begin(9600);
 }
 
-#define TASO1 180
-#define TASO2 100
+bool buttonIsPressed(){
+  bool retVal = false;
+  int x = digitalRead(PAINIKE);
+  if(x == LOW && oldState == HIGH){//Etsitään painikkeen painallus
+    retVal = true;
+  }
+  oldState = x;
+  delay(FILTER);
+  return retVal;
+}
+
 void loop(){
-  if(digitalRead(PAINIKE)==LOW){
-    analogWrite(LED, TASO2);  
-  }else{
-    digitalWrite(LED, HIGH);  
+  if(buttonIsPressed()){
+    Serial.println("Jeps");
   }
 }
